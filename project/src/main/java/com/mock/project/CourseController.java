e10d73ceb1f7176413b39d56088d9b51b135e3b7
binh <binh.do1120@hcmut.edu.vn>
2021-08-23 21:17:30 +0700

    add course

--- project/src/main/java/com/mock/project/CourseController.java
+++ project/src/main/java/com/mock/project/CourseController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,7 +26,7 @@ public class CourseController {
         return courseService.getCourseById(id);
     }
 
-    @RequestMapping("/courses/add")
+    @RequestMapping(value = "/courses/add", method = RequestMethod.POST)
     public Course addCourse(@RequestBody Course course) {
         return courseService.addCourse(course);
     }
