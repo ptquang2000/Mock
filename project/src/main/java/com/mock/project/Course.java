05a161ef4f602db33ea46030c96bee64814d4a0a
binh <binh.do1120@hcmut.edu.vn>
2021-08-23 20:59:25 +0700

    courses

--- project/src/main/java/com/mock/project/Course.java
+++ project/src/main/java/com/mock/project/Course.java
@@ -1,5 +1,49 @@
 package com.mock.project;
 
-public class Course {
-  
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course implements Serializable{
+    @Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Course() {}
+    
+    public Course(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "\nCourse{" +
+			"\n\tid='" + getId() + "'" +
+			",\n\tname='" + getName() + "'" +
+			"\n}";
+    }
 }
--- project/src/main/java/com/mock/project/CourseController.java
+++ project/src/main/java/com/mock/project/CourseController.java
@@ -1,5 +1,32 @@
 package com.mock.project;
 
+import java.util.List;
+
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CourseController {
+    @Autowired
+    private CourseService courseService;
+
+    @RequestMapping("/courses")
+    public List<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @RequestMapping("/courses/{id}")
+    public Course getCourseById(@RequestBody Long id) {
+        return courseService.getCourseById(id);
+    }
   
+    @ExceptionHandler(SQLServerException.class)
+    public String sqlExceptionHandler(SQLServerException e){
+        return e.getMessage();
+  }
 }
