ce80b429da8fdf4a0935d48a30437951919e8940
ptquang <ptquang2000@gmail.com>
2021-08-26 18:22:11 +0700

    fix quiz radios

--- project/src/main/resources/static/quiz.js
+++ project/src/main/resources/static/quiz.js
@@ -1,15 +1,18 @@
 const content = document.querySelector(".content")
 content.addEventListener('click', e => {
   const answer = e.target
-    if (answer.tagName.toLowerCase() == 'input') {
-      const question = e.target.closest('.question')
-      const correctAnswer = question.getAttribute('answer')
-      if (answer.value == correctAnswer){
-        answer.closest('label').style.color = '#04FF57'
-      }
-      else{
-        answer.closest('label').style.color = '#D6040D'
-        question.querySelector(`div.q-ans > label:nth-child(${correctAnswer})`).style.color = '#04FF57'
-      }
+  if (answer.tagName.toLowerCase() == 'input') {
+    const question = e.target.closest('.question')
+    const radios = question.querySelectorAll('input')
+    const correctAnswer = question.getAttribute('answer')
+    if (answer.value == correctAnswer){
+      answer.closest('label').style.color = '#04FF57'
+    }
+    else{
+      answer.closest('label').style.color = '#D6040D'
+      question.querySelector(`div.q-ans > label:nth-child(${correctAnswer})`).style.color = '#04FF57'
+    }
+    for (let i =0; i < radios.length; i++)
+      radios[i].disabled = true
   }
 })
