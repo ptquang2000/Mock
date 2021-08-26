9243f33a68ccdfe04d89aee6881f8e8921cdbeb6
ptquang <ptquang2000@gmail.com>
2021-08-27 01:13:51 +0700

    delete course

--- project/src/main/resources/static/js/courselist.js
+++ project/src/main/resources/static/js/courselist.js
@@ -2,11 +2,15 @@ const courseList = document.querySelector("body > div.courselist")
 courseList.addEventListener('click', e => {
   const selection = e.target.closest('.selection')
   const child = e.target
-  if ( selection.className == 'selection'){
-    window.location.href = `http://localhost:8080?lessonID=${selection.id}`
+  if (child.className == 'remove-btn'){
+    console.log('remove btn')
+    const question = e.target.closest('.selection')
+    const id = question.id
+    const form = question.querySelector('.removeCourse')
+    form.submit()
   }
-  if (child.className == 'remove btn'){
-    
+  else if (selection.className == 'selection'){
+    window.location.href = `http://localhost:8080?lessonID=${selection.id}`
   }
 })
 
